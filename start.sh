aef5dba2124e8a787ec7c15945933f571485b4d9
onyxcherry <50772375+onyxcherry@users.noreply.github.com>
2020-03-20 10:02:59 +0100

    Correct mistakes

--- start.sh
+++ start.sh
@@ -12,10 +12,12 @@ apt install curl htop vim mc
 
 apt update && apt upgrade 
 
+adduser $SUDO_USER
 usermod -aG sudo $SUDO_USER
 
 mkdir $HOME/.ssh
 #echo $PUBLIC_KEY > $HOME/.ssh/authorized_keys
+touch $HOME/.ssh/authorized_keys
 chmod go-w $HOME $HOME/.ssh
 chmod 600 $HOME/.ssh/authorized_keys
 chown $WHOAMI $HOME/.ssh/authorized_keys
@@ -23,15 +25,23 @@ systemctl restart sshd
 
 echo -e "syntax on\n set autoindent\n set background=dark\n set incsearch\n set hlsearch" >> $HOME/.vimrc
 
-echo "alias subdomainFoo='cd /var/www/html/sub.domain.com'
-alias exampleFoo='cd /var/www/html/example.com'
+echo "alias l.='ls -d .* --color=auto'
+#alias subdomainFoo='cd /var/www/html/sub.domain.com'
+#alias exampleFoo='cd /var/www/html/example.com'
+
+alias ports='netstat -tulanp'
+
 alias ..='cd ..'
 alias ...='cd ../../'
-alias ....='cd ../../../'" >> $HOME/.bashrc
+alias ....='cd ../../../' 
+alias .....='cd ../../../../'
+alias .4='cd ../../../../'
+alias .5='cd ../../../../..' ">> $HOME/.bashrc
+source $HOME/.bashrc
 
 #Install 2FA
 apt install libpam-google-authenticator && google-authenticator
-echo "auth required pam_google_authenticator.so" >> /etc/pam.d/sshd
+echo "auth requigroured pam_google_authenticator.so" >> /etc/pam.d/sshd
 #sed -i 's/ChallengeResponseAuthentication no/ChallengeResponseAuthentication yes/' /etc/ssh/sshd_config
 sed -i 's/UsePAM no/UsePAM yes/' /etc/ssh/sshd_config
 sed -i 's/\(ChallengeResponseAuthentication\) yes/\1 no/g' /etc/ssh/sshd_config
@@ -55,40 +65,30 @@ service sshd restart
 #rclone config
 
 echo "#!/bin/bash
-
  rm -rf /backup/starszy
  mv /backup/nowszy/* /backup/starszy/
  mkdir /backup/nowszy
-
  mkdir -p /backup/from_root
-
  cp -aR -- .config /root/backup/from_root
  cp -aR -- /root/.[bglmnpsvw]* /backup/from_root/
-
  TIME=$(date +"%m-%d-%Y")
-
  FILENAME=backup-root-$TIME.tar.gz
  SRCDIR=/backup/from_root/
  DESDIR=/backup/nowszy/
  tar -cvpzf $DESDIR/$FILENAME $SRCDIR
-
  FILENAME=backup-etc-$TIME.tar.gz
  SRCDIR=/etc/
  DESDIR=/backup/nowszy/
  tar -cvpzf $DESDIR/$FILENAME $SRCDIR
-
  FILENAME=backup-var-www--$TIME.tar.gz
  SRCDIR=/var/www/
  DESDIR=/backup/nowszy/
  tar -cvpzf $DESDIR/$FILENAME $SRCDIR
-
  FILENAME=backup-var-log-$TIME.tar.gz
  SRCDIR=/var/log/
  DESDIR=/backup/nowszy/
  tar -cvpzf $DESDIR/$FILENAME $SRCDIR
-
  rm -rf /backup/from_root/.[^.]*
-
  #rclone copy /backup/nowszy/ mega:server  --buffer-size 0M" > /root/backup.sh
 
  chmod +x backup.sh
